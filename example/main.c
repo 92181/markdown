3fa0ec17e5cf499c97954c7b88c4bc4bdd3543cf
Wolf Schulz <82281132+92181@users.noreply.github.com>
2025-05-03 00:19:02 +0200

    Add files via upload

---
+++ example/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "../md.c"
+
+struct stat d;
+
+// Main Logic;
+int main(int e,char **h)
+{
+  // Check For Valid Usage;
+  if(e<2)
+  {
+    char i[]="Correct Usage: ./main page.md\n";write(1,i,sizeof(i));
+
+    return 0;
+  };
+
+  // Load Markup File;
+  unsigned int f=open(*(h+1),0),u,s,l;
+
+  if(f>>31)
+  {
+    return 1;
+  };
+
+  fstat(f,&d);s=d.st_size;
+  char *b=malloc(s),*r;
+  
+  read(f,b,s);
+
+  // Parse File And Write Result To File;
+  r=psr(&u,b,s);l=open("index.html",O_CREAT|O_TRUNC|O_WRONLY,420);
+
+  if(l>>31)
+  {
+    return 1;
+  };
+
+  printf("Resulting File Size: %u\n",u);
+  printf("first char: %c\n",*r);
+
+  write(l,r,u); // r is not correct!
+
+  // Return Success;
+  return 0;
+
+  // Free Memory;
+  close(f);close(l);free(b);
+};
---
+++ example/page.md
@@ -0,0 +1,14 @@
+# Hello
+New neighbours are always a *happy* surprise?damn
+
+# hey stranger 
+
+what can i do for you?
+
+> indentation is for real
+
+* List
+* another item
+* second item
+
+tables i am not even gonna try
